27b6e7d1d37a79a823ae5c72bf137f286ff69253
benedictmouse <152507362+benedictmouse@users.noreply.github.com>
2024-06-11 15:17:36 -0700

    navbar
    to help in navigation

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -4,21 +4,25 @@ import { appLinks } from "../constants/Links";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ width: "100%" }}>
       <div className="container-fluid">
-        <Link to={appLinks?.Landing} className="navbar-brand">
-          Bentech
-        </Link>
-        <div className="collapse navbar-collapse" id="navbarText">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            
             <li className="nav-item">
-              <Link className="nav-link" to={appLinks?.Contact}>
+              <Link to={appLinks?.Landing} className="nav-link active" aria-current="page">
+                Bentech
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to={appLinks?.Contact}>
                 Contact Us
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={appLinks?.About}>
+              <Link className="nav-link active " to={appLinks?.About}>
                 About
               </Link>
             </li>
